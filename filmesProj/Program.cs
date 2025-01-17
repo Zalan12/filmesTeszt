79841446d7b0ee76c9e32c6ccf74ded7ba4cfe4d
Zalan12 <farkaszalan@turr.hu>
2025-01-17 09:33:21 +0100

    Tesztes

---
+++ filmesProj/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace filmesProj
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int id = 0;
+            for (int i = 0; i < 4; i++) 
+            {
+                Console.WriteLine(id);
+            }
+        }
+    }
+}
